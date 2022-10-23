64d611d6119938621d7fb07630e41bb02e1bbb30
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-23 14:19:59 +0100

    basic routing now functional

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+// register view engine
+app.set("view engine", "ejs");
+
 // middleware & static files
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "welcome to the app",
+  });
+});
+
+// Listen for requests
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(` Server listening on port ${port}`);
+});
